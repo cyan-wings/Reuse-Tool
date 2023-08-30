f297b5f23c6db2112342f6cd043860fb3e7fb041
cyan-wings <106809287+cyan-wings@users.noreply.github.com>
2023-08-30 18:34:53 +0800

    Update About.py

--- About.py
+++ About.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import os
 
+@st.cache_data
 def intro():
 
+    os.system("wget https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz")
+    os.system("tar zxvf openjdk-11+28_linux-x64_bin.tar.gz")
+    os.environ['JAVA_HOME'] = "jdk-11"
+    os.environ['PATH'] = os.environ.get('PATH') + ':jdk-11/bin'
+    os.system("java -version")
+    
     st.write("# Machine Learning-Assisted Software Reuse Prediction and Analysis")
     st.sidebar.header("About")
 
